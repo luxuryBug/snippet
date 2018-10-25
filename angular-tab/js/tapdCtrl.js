b686a7d4b047d9e32d68676c9c4234804cdd2881
guoluo <guoluo@acewill.cn>
2018-10-25 14:15:56 +0800

    实现angular 多页签

---
+++ angular-tab/js/tapdCtrl.js
@@ -0,0 +1,15 @@
+
+(function(){
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('tapdCtrl', tapdCtrl)
+
+    /** @ngInject */
+    function tapdCtrl($scope){
+        console.log('tapdCtrl--start')
+    }
+
+}());
+
